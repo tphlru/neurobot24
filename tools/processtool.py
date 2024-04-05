48e8c638ae5477d5b330eb736759174e24e71a62
tphlrussia <35965490+tphlrussia@users.noreply.github.com>
2024-04-05 15:02:39 +0300

    Remove redundant color space conversion in histogram_adp

--- tools/processtool.py
+++ tools/processtool.py
@@ -4,52 +4,41 @@ import cv2
 import numpy as np
 
 
-def mask_by_color(img, *args):
+def mask_by_color(img, ranges):
     """
-    Generate masks for given color ranges and return the merged mask.
+    Generate a mask for an image based on color ranges.
 
-    Args:
-        img: Input image in BGR format.
-        *args: Color ranges in the format of tuples or lists.
+    Parameters:
+    - img (numpy.ndarray): The image to mask
+    - ranges (list/tuple/numpy.ndarray): Color ranges for masking
 
     Returns:
-        Merged mask containing all color ranges.
+    - numpy.ndarray: The resulting mask
+
+    This function converts the image to HSV for color masking. Supports both color and grayscale images.
     """
+    if not isinstance(ranges, list):
+        ranges = list(ranges)
 
-    if not args:
-        raise ValueError("At least one color range is required")
+    if not any(isinstance(x, list) for x in ranges):
+        ranges = [ranges]
 
-    try:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    except Exception:
+    if len(img.shape) != 3:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    ranges_list = []
-
-    # check if each of args values are tuple (convert each to list) or list (save as it is)
-    # or raise TypeError
-    for arg in args:
-        if isinstance(arg, tuple):
-            arg = list(arg)
-        elif not isinstance(arg, list):
-            raise TypeError(f"Invalid color range type: {type(arg)}")
-
-        ranges_list.append(
-            np.array(arg)
-        )  # Convert to [[np-low, np-high], [np-low, np-high] ...]
+    ranges_list = [list(map(np.array, rang)) for rang in ranges]
 
     masks_list = []
-
-    # create masks for each color range
     for ranges in ranges_list:
         mask = cv2.inRange(
-            img, ranges[0], ranges[1]
-        )  # create mask for each color range
-        masks_list.append(mask)  # append mask to masks_list
+            img,
+            ranges[0],
+            ranges[1],
+        )
+        masks_list.append(mask)
 
-    # merge all masks
-    masks_merged = masks_list[0]  # first mask
+    masks_merged = masks_list[0]
     if len(masks_list) > 1:
         for i in range(1, len(masks_list)):
             masks_merged = cv2.bitwise_or(masks_merged, masks_list[i])
@@ -70,24 +59,31 @@ def mask_center(img, wd=None, ht=None, divk=2):
     """
     ih, iw = img.shape[:2]
     cmask = np.zeros((ih, iw, 3), dtype=np.uint8)
-    if wd and ht:
+    
+    if wd is not None and ht is not None:
         divk = None
         center_area_height = ht
         center_area_width = wd
     else:
-        center_area_height = round(ih / divk)
-        center_area_width = round(iw / divk)
+        center_area_height = ih // divk
+        center_area_width = iw // divk
+    
     top_offset = (ih - center_area_height) // 2
     left_offset = (iw - center_area_width) // 2
-    print(center_area_height, center_area_width)
+    
     cmask[
-        top_offset : top_offset + center_area_height,
-        left_offset : left_offset + center_area_width,
+    slice(top_offset, top_offset + center_area_height),
+    slice(left_offset, left_offset + center_area_width),
     ] = (255, 255, 255)
+    
     return cmask
 
 
-def get_dist_to_center(a, b):
+def invert(img):
+    return 255 - img
+
+
+def get_dist_to_center(a, b) -> float:
     """
     Calculate the distance from the point (a, b) to the center (0, 0) in a 2D plane.
 
@@ -99,4 +95,4 @@ def get_dist_to_center(a, b):
         float: The distance from the point to the center, rounded to 2 decimal places.
     """
     a, b = abs(a), abs(b)
-    return int(round(sqrt(a**2 + b**2)))
+    return round(sqrt(a ** 2 + b ** 2), 2)
