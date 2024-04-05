f5a1561ec2ebe09165647cb68576eab62982d27f
tphlrussia <35965490+tphlrussia@users.noreply.github.com>
2024-04-05 15:01:48 +0300

    Enhancements, Modularize brightness and contrast adjustment

--- tools/filters.py
+++ tools/filters.py
@@ -1,6 +1,28 @@
 import cv2
 
-def bright_contrast(input_img, brightness = 0, contrast = 0):
+
+def adjust_brightness(input_img, brightness):
+    if brightness > 0:
+        shadow = brightness
+        highlight = 255
+    else:
+        shadow = 0
+        highlight = 255 + brightness
+    alpha_b = (highlight - shadow) / 255
+    gamma_b = shadow
+
+    return cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
+
+
+def adjust_contrast(input_img, contrast):
+    f = 131 * (contrast + 127) / (127 * (131 - contrast))
+    alpha_c = f
+    gamma_c = 127 * (1 - f)
+
+    return cv2.addWeighted(input_img, alpha_c, input_img, 0, gamma_c)
+
+
+def bright_contrast(input_img, brightness=0, contrast=0):
     """
     Apply brightness and contrast adjustments to an input image.
 
@@ -12,33 +34,18 @@ def bright_contrast(input_img, brightness = 0, contrast = 0):
     Returns:
         The processed image with brightness and contrast adjustments applied.
     """
-    
+    buf = input_img.copy()
+
     if brightness != 0:
-        if brightness > 0:
-            shadow = brightness
-            highlight = 255
-        else:
-            shadow = 0
-            highlight = 255 + brightness
-        alpha_b = (highlight - shadow)/255
-        gamma_b = shadow
-        
-        buf = cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
-    else:
-        buf = input_img.copy()
-    
+        buf = adjust_brightness(buf, brightness)
+
     if contrast != 0:
-        f = 131*(contrast + 127)/(127*(131-contrast))
-        alpha_c = f
-        gamma_c = 127*(1-f)
-        
-        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+        buf = adjust_contrast(buf, contrast)
 
     return buf
 
 
-
-def histogram_adp(img, cllimit=1.0, gridsize=(6,6)):
+def histogram_adp(img, cllimit=1.0, gridsize=(6, 6)):
     """
     Adaptive histogram equalization for brightness channel only (Y)
 
@@ -55,7 +62,7 @@ def histogram_adp(img, cllimit=1.0, gridsize=(6,6)):
     # https://en.wikipedia.org/wiki/Adaptive_histogram_equalization
 
     clahe = cv2.createCLAHE(clipLimit=cllimit, tileGridSize=gridsize)
-    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV) # convert to YUV
-    img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0]) # equalize the Y channel
-    img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR) # convert to BGR
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)  # convert to YUV
+    img_yuv[:, :, 0] = clahe.apply(img_yuv[:, :, 0])  # equalize the Y channel
+    img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)  # convert to BGR
     return img
